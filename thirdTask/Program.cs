9cdbb9b18baea14fb75c587b8c49a4e1daaef00a
artemmar <artemmarcoop@gmail.com>
2020-09-30 12:27:58 +0300

    initial commit

---
+++ thirdTask/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace thirdTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            double b;
+            long c;
+            Console.WriteLine("Введіть змінну типу Int ");
+            while (true)
+            {
+                try
+                {
+                    Console.Write(" a = ");
+                    a = Int32.Parse(Console.ReadLine());
+                    break;
+                } catch (Exception e)
+                {
+                    Console.WriteLine("Ви ввели неккоректні дані!!! Спробуйте знову");
+                }
+            }
+            Console.WriteLine("Введіть змінну типу double ");
+            while (true)
+            {
+                try
+                {
+                    Console.Write(" b = ");
+                    b = Convert.ToDouble(Console.ReadLine());
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Ви ввели неккоректні дані!!! Спробуйте знову");
+                }
+            }
+            Console.WriteLine("Введіть змінну типу long ");
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write(" c = ");
+                    c = Int64.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Ви ввели неккоректні дані!!! Спробуйте знову");
+                }
+            }
+            Console.WriteLine("a = " + a + "; b = " + b + "; c = " + c);
+            Console.ReadKey();
+        }
+    }
+}
